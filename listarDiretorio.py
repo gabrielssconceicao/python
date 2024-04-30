c4f81352f848f5c7f781e7b5f052df3edf76a884
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-04-30 16:37:12 -0300

    separating functions

--- listarDiretorio.py
+++ listarDiretorio.py
@@ -1,7 +1,8 @@
 import os
 
 
-def list_files_and_dirs_tail_recursive(path, level=1, content=None,arquivos=None):
+def list_files_and_dirs_tail_recursive(path, level=1, content=None,
+                                       arquivos=None):
     if content is None:
         content = os.listdir(path)
     if len(content) == 0:
@@ -12,22 +13,49 @@ def list_files_and_dirs_tail_recursive(path, level=1, content=None,arquivos=None
     # Pegar o primeiro item do array content
     item = content.pop(0)
     path_item = os.path.join(path, item)
-   
     
-    #Verifica se é diretorio
+    # Verifica se é diretorio
     if os.path.isdir(path_item):
         new_files = os.listdir(path_item)
-        arquivos.append({"file": item+"/", "level": level})
-        result = list_files_and_dirs_tail_recursive(path_item,level+1,new_files)
-        arquivos.extend(result)
+        arquivos.append({"file": item + "/", "level": level})
+        result = list_files_and_dirs_tail_recursive(path_item, level + 1,
+                                                    new_files)
+        if result:
+            arquivos.extend(result)
     else:
         arquivos.append({"file": item, "level": level})
-    return list_files_and_dirs_tail_recursive(path,level,content,arquivos)
-    
-    
+    return list_files_and_dirs_tail_recursive(path, level, content, arquivos)
 
-directory_path = ""
-files = list_files_and_dirs_tail_recursive(directory_path)
-print(f"Arquivos de {directory_path}")
-for file in files:
-    print("\t" * file["level"] + file["file"])
+
+def verify_directory_path():
+    directory_path = ""
+    while not os.path.isdir(directory_path):
+        directory_path = input("Digite um diretório: ")
+    
+    return directory_path
+
+
+def list_files(directory_path):
+    files = list_files_and_dirs_tail_recursive(directory_path)
+    print(f"Arquivos de {directory_path}")
+    for file in files:
+        print("\t" * file["level"] + file["file"])
+
+
+def continue_prompt():
+    again = ""
+    while again not in "sn":
+        again = input("Quer ir de novo? [S/N]").strip().lower()
+    return again == "s"
+
+
+def main():
+    while True:
+        directory_path = verify_directory_path()
+        list_files(directory_path)
+        again = continue_prompt()
+        if again != "s":
+            break
+
+
+main()
