a0782a71acbd400d5cbe2d5da02cc976e9b68251
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-04-30 16:13:23 -0300

    finishing code

--- listarDiretorio.py
+++ listarDiretorio.py
@@ -1,18 +1,33 @@
 import os
 
 
-def list_files_and_dirs_tail_recursive(path, level=1, content=None):
+def list_files_and_dirs_tail_recursive(path, level=1, content=None,arquivos=None):
     if content is None:
         content = os.listdir(path)
     if len(content) == 0:
-        return
+        return arquivos
+    if arquivos is None:
+        arquivos = []
     
-    # Pega o
-    print(path)
-    print(content)
+    # Pegar o primeiro item do array content
+    item = content.pop(0)
+    path_item = os.path.join(path, item)
+   
+    
+    #Verifica se é diretorio
+    if os.path.isdir(path_item):
+        new_files = os.listdir(path_item)
+        arquivos.append({"file": item+"/", "level": level})
+        result = list_files_and_dirs_tail_recursive(path_item,level+1,new_files)
+        arquivos.extend(result)
+    else:
+        arquivos.append({"file": item, "level": level})
+    return list_files_and_dirs_tail_recursive(path,level,content,arquivos)
+    
+    
 
-
-# Exemplo de uso:
-directory_path = 'C:\\Users\gabri\Documents\Python'
-directory_path2 = 'C:\Development\Tools\Java'
-list_files_and_dirs_tail_recursive(directory_path2)
+directory_path = ""
+files = list_files_and_dirs_tail_recursive(directory_path)
+print(f"Arquivos de {directory_path}")
+for file in files:
+    print("\t" * file["level"] + file["file"])
